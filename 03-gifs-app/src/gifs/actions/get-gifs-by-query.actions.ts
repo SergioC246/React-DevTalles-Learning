867ba25ad367150ffa9072f4056b322867b85280
Sergio Córdoba <sergiocordoba246@gmail.com>
2026-04-14 13:54:04 +0200

    adding useEffect

---
+++ 03-gifs-app/src/gifs/actions/get-gifs-by-query.actions.ts
@@ -0,0 +1,20 @@
+import type { GihpyResponse } from '../interfaces/giphy.respose';
+import type { Gif } from '../interfaces/gif.interface';
+import { gihpyApi } from '../api/gihpy.api';
+
+export const getGifsByQuery = async(query: string): Promise<Gif[]> => {
+  const response = await gihpyApi<GihpyResponse>('/search', {
+    params: {
+      q:query,
+      limit:10,         
+    },
+  });
+
+  return response.data.data.map((gif) => ({
+    id: gif.id,
+    title: gif.title,
+    url: gif.images.original.url,
+    width: Number(gif.images.original.width),
+    height: Number(gif.images.original.height),
+  }));
+};
