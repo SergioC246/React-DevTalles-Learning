8445cfff462e27cc0178e581259b274c17932e9c
Sergio Córdoba <sergiocordoba246@gmail.com>
2026-04-21 14:05:14 +0200

    useReducer

--- 04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
+++ 04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
@@ -1,3 +1,5 @@
+import * as z from "zod";
+
 interface Todo {
   id: number;
   text: string;
@@ -14,12 +16,99 @@ interface TaskState {
 export type TaskAction = 
   | { type: 'ADD_TODO'; payload: string }
   | { type: 'TOGGLE_TODO'; payload: number }
-  | { type: 'DELETE_TODO'; payload: number };
+  | { type: 'DELETE_TODO'; payload: number }
+
+const TodoSchema = z.object({
+  id: z.number(), 
+  text: z.string(), 
+  completed: z.boolean(),
+});
+
+const TaskStateSchema = z.object({
+  todos: z.array(TodoSchema), 
+  length: z.number(),
+  completed: z.number(),
+  pending: z.number(),
+}); 
+
+
+  
+export const getTasksInitialState = (): TaskState => {
+  const localStorageState = localStorage.getItem('task-state');
+
+  if (!localStorageState) {
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };    
+  }
+
+  const result = TaskStateSchema.safeParse(JSON.parse(localStorageState));
+
+  if ( result.error ) {
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };
+  }
+  return result.data;
+};
   
 
 export const taskReducer = (
   state: TaskState,
   action: TaskAction
 ): TaskState => {
-  return state;
+  switch (action.type) {
+
+    case 'ADD_TODO': {
+      const newTodo: Todo = {
+      id: Date.now(),
+      text: action.payload,
+      completed: false,
+      };
+
+      return {
+        ...state,
+        todos: [...state.todos, newTodo],
+        length: state.todos.length + 1,
+        pending: state.pending + 1,
+      };
+    }
+
+    case 'DELETE_TODO': {
+      const currentTodos = state.todos.filter((todo) => todo.id !== action.payload)
+
+      return {
+        ...state,
+        todos: currentTodos,
+        length: currentTodos.length,
+        completed: currentTodos.filter(todo => todo.completed).length,
+        pending: currentTodos.filter(todo => !todo.completed).length,
+      };
+    }
+      
+    case 'TOGGLE_TODO': {
+      const updatedTodos = state.todos.map((todo) => {
+      if (todo.id === action.payload) {
+        return { ...todo, completed: !todo.completed }
+      }
+      return todo;
+    });
+
+      return {
+        ...state,
+        todos: updatedTodos,
+        completed: updatedTodos.filter(todo => todo.completed).length,
+        pending: updatedTodos.filter(todo => !todo.completed).length,
+      };
+    }
+
+    default:
+      return state;
+  }
 };
